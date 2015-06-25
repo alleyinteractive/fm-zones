e2461c5eb26701e5d97aea0f7c395fdfb4eac21e
Matt Boynes <mboynes@alleyinteractive.com>
2015-06-25 16:20:48 -0400

    Credit where credit is due
    Add an explicit note that this is a derivative work of Zoninator.

--- fm-zone.php
+++ fm-zone.php
@@ -8,7 +8,15 @@
 	Author: Alley Interactive
 	Author URI: http://www.alleyinteractive.com/
 */
-/*  This program is free software; you can redistribute it and/or modify
+/*
+    Copyright 2010-2015 Mohammad Jangda, Automattic
+    Copyright 2015 Alley Interactive
+
+    The following code is a derivative work of code from the Automattic plugin
+    Zoninator, which is licensed GPLv2. This code therefore is also licensed
+    under the terms of the GNU Public License, verison 2.
+
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
