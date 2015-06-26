ec65986ab80caa286534bd9e0d0b3e347867bde3
Matt Boynes <mboynes@alleyinteractive.com>
2015-06-25 20:56:58 -0400

    Convert indent to tabs

--- fm-zones.php
+++ fm-zones.php
@@ -9,26 +9,26 @@
 	Author URI: http://www.alleyinteractive.com/
 */
 /*
-    Copyright 2010-2015 Mohammad Jangda, Automattic
-    Copyright 2015 Alley Interactive
+	Copyright 2010-2015 Mohammad Jangda, Automattic
+	Copyright 2015 Alley Interactive
 
-    The following code is a derivative work of code from the Automattic plugin
-    Zoninator, which is licensed GPLv2. This code therefore is also licensed
-    under the terms of the GNU Public License, verison 2.
+	The following code is a derivative work of code from the Automattic plugin
+	Zoninator, which is licensed GPLv2. This code therefore is also licensed
+	under the terms of the GNU Public License, verison 2.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 define( 'FMZ_PATH', dirname( __FILE__ ) );
