afe4bf8e921074ea5c08da59853034403f9b0a51
Matt Boynes <mboynes@alleyinteractive.com>
2015-07-31 10:31:08 -0400

    Remove closure and abort more efficiently

--- php/class-fieldmanager-zone-field.php
+++ php/class-fieldmanager-zone-field.php
@@ -1,207 +1,203 @@
 <?php
 
-if ( class_exists( 'Fieldmanager_Field' ) && ! class_exists( 'Fieldmanager_Zone_Field' ) ) {
+/**
+ * Zoninator Act-Alike Fieldmanager Field
+ */
+class Fieldmanager_Zone_Field extends Fieldmanager_Field {
 
-	/**
-	 * Zoninator Act-Alike Fieldmanager Field
-	 */
-	class Fieldmanager_Zone_Field extends Fieldmanager_Field {
+	private $field_id;
 
-		private $field_id;
+	public $query_args = array();
 
-		public $query_args = array();
+	public $autocomplete_attributes = array();
 
-		public $autocomplete_attributes = array();
+	public $accept_from_other_zones = false;
 
-		public $accept_from_other_zones = false;
+	public $ajax_args = array();
 
-		public $ajax_args = array();
+	public function __construct( $label = '', $options = array() ) {
+		$this->template = FMZ_PATH . '/templates/field.php';
+		$this->multiple = true;
+		$this->sanitize = 'absint';
 
-		public function __construct( $label = '', $options = array() ) {
-			$this->template = FMZ_PATH . '/templates/field.php';
-			$this->multiple = true;
-			$this->sanitize = 'absint';
+		parent::__construct( $label, $options );
 
-			parent::__construct( $label, $options );
-
-			add_action( 'admin_enqueue_scripts', array( $this, 'assets' ) );
-			add_action( 'wp_ajax_' . $this->get_ajax_action(), array( $this, 'ajax_request' ) );
-		}
-
-		public function assets() {
-			wp_enqueue_style( 'fm-zone-jquery-ui', FMZ_URL . '/static/jquery-ui/smoothness/jquery-ui.theme.css', false, FMZ_VERSION, 'all' );
-			wp_enqueue_style( 'fm-zone-styles', FMZ_URL . '/static/css/fm-zone.css', false, FMZ_VERSION, 'all' );
-			wp_enqueue_script( 'fm-zone-script', FMZ_URL . '/static/js/fm-zone.js', array( 'jquery', 'underscore', 'jquery-ui-sortable', 'jquery-ui-autocomplete' ), FMZ_VERSION, true );
-		}
-
-		public function form_element( $value = null ) {
-			list( $context, $subcontext ) = fm_get_context();
-			$this->autocomplete_attributes['data-context'] = $context;
-			$this->autocomplete_attributes['data-subcontext'] = $subcontext;
-			$this->autocomplete_attributes['data-action'] = $this->get_ajax_action( $this->name );
-			$this->autocomplete_attributes['data-nonce'] = wp_create_nonce( 'fm_search_nonce' );
-			$this->autocomplete_attributes['data-args'] = json_encode( $this->ajax_args );
-
-			return parent::form_element( $value );
-		}
-
-		/**
-		 * Generates an HTML attribute string based on the value of
-		 * $this->autocomplete_attributes.
-		 *
-		 * @see Fieldmanager_Field::$autocomplete_attributes
-		 * @return string HTML attributes ready to insert into an HTML tag.
-		 */
-		public function get_element_autocomplete_attributes() {
-			$attr_str = array();
-			foreach ( $this->autocomplete_attributes as $attr => $val ) {
-				if ( $val === true ){
-					$attr_str[] = sanitize_key( $attr );
-				} else{
-					$attr_str[] = sprintf( '%s="%s"', sanitize_key( $attr ), esc_attr( $val ) );
-				}
-			}
-			return implode( ' ', $attr_str );
-		}
-
-		/**
-		 * Get the most recent posts.
-		 *
-		 * @todo limit to certain time period (last 3 months?) by default?
-		 *
-		 * @param  array  $except Post IDs to exclude, because they're already
-		 *                        in the zone.
-		 * @return array {@see Fieldmanager_Zone_Field::format_posts()}
-		 */
-		public function get_recent_posts( $except = array() ) {
-			return $this->get_posts( array(
-				'post__not_in' => $except,
-				'orderby' => 'date',
-				'order' => 'desc',
-			) );
-		}
-
-		/**
-		 * Get an array of posts matching default and given criteria.
-		 *
-		 * @todo limit to last year(?) by default for performance, make that an
-		 *       option.
-		 *
-		 * @param  array  $args WP_Query args
-		 * @return array {@see Fieldmanager_Zone_Field::format_posts()}
-		 */
-		public function get_posts( $args = array() ) {
-			$args = array_merge(
-				array(
-					'post_type' => 'post',
-					'post_status' => 'publish',
-					'posts_per_page' => 10,
-				),
-				$this->query_args,
-				$args
-			);
-
-			return $this->format_posts( get_posts( $args ) );
-		}
-
-		public function format_posts( $posts, $format = 'array' ) {
-			$return = array();
-
-			foreach ( $posts as $post ) {
-				/**
-				 * @todo filter this so that the output can be customized
-				 */
-				$return[] = array(
-					'id'     => $post->ID,
-					'post_status' => $post->post_status,
-					'post_type'   => $post->post_type,
-					'title'  => $post->post_title,
-					'date'   => $post->post_date,
-					'thumb'  => has_post_thumbnail( $post->ID ) ? get_the_post_thumbnail( $post->ID, array( 50, 50 ) ) : '',
-					'link'   => get_permalink( $post->ID ),
-				);
-			}
-
-			if ( 'json' == $format ) {
-				return json_encode( $return );
-			} else {
-				return $return;
-			}
-		}
-
-		public function get_current_posts_json( $ids ) {
-			if ( empty( $ids ) ) {
-				return '[]';
-			} else {
-				return $this->format_posts(
-					get_posts( array(
-						'post__in' => $ids,
-						'post_status' => 'any',
-						'post_type' => 'any',
-						'orderby' => 'post__in',
-						'order' => 'asc',
-					) ),
-					'json'
-				);
-			}
-		}
-
-		public function get_ajax_action() {
-			return $this->get_field_id() . '_search_ajax';
-		}
-
-		public function get_field_id() {
-			if ( ! $this->field_id ) {
-				$el = $this;
-				$id_slugs = array();
-				while ( $el ) {
-					array_unshift( $id_slugs, $el->name );
-					$el = $el->parent;
-				}
-				$this->field_id = 'fm-' . implode( '-', $id_slugs );
-			}
-			return $this->field_id;
-		}
-
-		public function ajax_request() {
-			check_ajax_referer( 'fm_search_nonce', '_nonce' );
-
-			if ( empty( $_POST['term'] ) ) {
-				wp_send_json_error( __( 'Search term is empty!', 'fm-zones' ) );
-			}
-
-			$args = array(
-				's' => sanitize_text_field( $_POST['term'] ),
-			);
-			if ( ! empty( $_POST['exclude'] ) ) {
-				$args['post__not_in'] = array_map( 'intval', (array) $_POST['exclude'] );
-			}
-			$posts = $this->get_posts( $args );
-
-			wp_send_json_success( $posts );
-		}
-
-		public function maybe_connect() {
-			if ( $this->accept_from_other_zones ) {
-				echo ' fm-zone-posts-connected';
-			}
-		}
-
-		/**
-		 * Presave function, which handles sanitization and validation
-		 *
-		 * @param int|array $values This will either be a post ID or array of
-		 *                          post IDs.
-		 * @return int|array Sanitized values.
-		 */
-		public function presave( $values, $current_values = array() ) {
-			if ( is_array( $values ) ) {
-				return array_map( $this->sanitize, $values );
-			} else {
-				return call_user_func( $this->sanitize, $values );
-			}
-		}
-
+		add_action( 'admin_enqueue_scripts', array( $this, 'assets' ) );
+		add_action( 'wp_ajax_' . $this->get_ajax_action(), array( $this, 'ajax_request' ) );
 	}
 
-} // if Fieldmanager_Field exxists
+	public function assets() {
+		wp_enqueue_style( 'fm-zone-jquery-ui', FMZ_URL . '/static/jquery-ui/smoothness/jquery-ui.theme.css', false, FMZ_VERSION, 'all' );
+		wp_enqueue_style( 'fm-zone-styles', FMZ_URL . '/static/css/fm-zone.css', false, FMZ_VERSION, 'all' );
+		wp_enqueue_script( 'fm-zone-script', FMZ_URL . '/static/js/fm-zone.js', array( 'jquery', 'underscore', 'jquery-ui-sortable', 'jquery-ui-autocomplete' ), FMZ_VERSION, true );
+	}
+
+	public function form_element( $value = null ) {
+		list( $context, $subcontext ) = fm_get_context();
+		$this->autocomplete_attributes['data-context'] = $context;
+		$this->autocomplete_attributes['data-subcontext'] = $subcontext;
+		$this->autocomplete_attributes['data-action'] = $this->get_ajax_action( $this->name );
+		$this->autocomplete_attributes['data-nonce'] = wp_create_nonce( 'fm_search_nonce' );
+		$this->autocomplete_attributes['data-args'] = json_encode( $this->ajax_args );
+
+		return parent::form_element( $value );
+	}
+
+	/**
+	 * Generates an HTML attribute string based on the value of
+	 * $this->autocomplete_attributes.
+	 *
+	 * @see Fieldmanager_Field::$autocomplete_attributes
+	 * @return string HTML attributes ready to insert into an HTML tag.
+	 */
+	public function get_element_autocomplete_attributes() {
+		$attr_str = array();
+		foreach ( $this->autocomplete_attributes as $attr => $val ) {
+			if ( $val === true ){
+				$attr_str[] = sanitize_key( $attr );
+			} else{
+				$attr_str[] = sprintf( '%s="%s"', sanitize_key( $attr ), esc_attr( $val ) );
+			}
+		}
+		return implode( ' ', $attr_str );
+	}
+
+	/**
+	 * Get the most recent posts.
+	 *
+	 * @todo limit to certain time period (last 3 months?) by default?
+	 *
+	 * @param  array  $except Post IDs to exclude, because they're already
+	 *                        in the zone.
+	 * @return array {@see Fieldmanager_Zone_Field::format_posts()}
+	 */
+	public function get_recent_posts( $except = array() ) {
+		return $this->get_posts( array(
+			'post__not_in' => $except,
+			'orderby' => 'date',
+			'order' => 'desc',
+		) );
+	}
+
+	/**
+	 * Get an array of posts matching default and given criteria.
+	 *
+	 * @todo limit to last year(?) by default for performance, make that an
+	 *       option.
+	 *
+	 * @param  array  $args WP_Query args
+	 * @return array {@see Fieldmanager_Zone_Field::format_posts()}
+	 */
+	public function get_posts( $args = array() ) {
+		$args = array_merge(
+			array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'posts_per_page' => 10,
+			),
+			$this->query_args,
+			$args
+		);
+
+		return $this->format_posts( get_posts( $args ) );
+	}
+
+	public function format_posts( $posts, $format = 'array' ) {
+		$return = array();
+
+		foreach ( $posts as $post ) {
+			/**
+			 * @todo filter this so that the output can be customized
+			 */
+			$return[] = array(
+				'id'     => $post->ID,
+				'post_status' => $post->post_status,
+				'post_type'   => $post->post_type,
+				'title'  => $post->post_title,
+				'date'   => $post->post_date,
+				'thumb'  => has_post_thumbnail( $post->ID ) ? get_the_post_thumbnail( $post->ID, array( 50, 50 ) ) : '',
+				'link'   => get_permalink( $post->ID ),
+			);
+		}
+
+		if ( 'json' == $format ) {
+			return json_encode( $return );
+		} else {
+			return $return;
+		}
+	}
+
+	public function get_current_posts_json( $ids ) {
+		if ( empty( $ids ) ) {
+			return '[]';
+		} else {
+			return $this->format_posts(
+				get_posts( array(
+					'post__in' => $ids,
+					'post_status' => 'any',
+					'post_type' => 'any',
+					'orderby' => 'post__in',
+					'order' => 'asc',
+				) ),
+				'json'
+			);
+		}
+	}
+
+	public function get_ajax_action() {
+		return $this->get_field_id() . '_search_ajax';
+	}
+
+	public function get_field_id() {
+		if ( ! $this->field_id ) {
+			$el = $this;
+			$id_slugs = array();
+			while ( $el ) {
+				array_unshift( $id_slugs, $el->name );
+				$el = $el->parent;
+			}
+			$this->field_id = 'fm-' . implode( '-', $id_slugs );
+		}
+		return $this->field_id;
+	}
+
+	public function ajax_request() {
+		check_ajax_referer( 'fm_search_nonce', '_nonce' );
+
+		if ( empty( $_POST['term'] ) ) {
+			wp_send_json_error( __( 'Search term is empty!', 'fm-zones' ) );
+		}
+
+		$args = array(
+			's' => sanitize_text_field( $_POST['term'] ),
+		);
+		if ( ! empty( $_POST['exclude'] ) ) {
+			$args['post__not_in'] = array_map( 'intval', (array) $_POST['exclude'] );
+		}
+		$posts = $this->get_posts( $args );
+
+		wp_send_json_success( $posts );
+	}
+
+	public function maybe_connect() {
+		if ( $this->accept_from_other_zones ) {
+			echo ' fm-zone-posts-connected';
+		}
+	}
+
+	/**
+	 * Presave function, which handles sanitization and validation
+	 *
+	 * @param int|array $values This will either be a post ID or array of
+	 *                          post IDs.
+	 * @return int|array Sanitized values.
+	 */
+	public function presave( $values, $current_values = array() ) {
+		if ( is_array( $values ) ) {
+			return array_map( $this->sanitize, $values );
+		} else {
+			return call_user_func( $this->sanitize, $values );
+		}
+	}
+
+}
